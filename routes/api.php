9eacda0c6f9452cb2ec75b46051c251d87a53200
Md. Rubayet Islam <rubayetislam16@gmail.com>
2025-10-07 19:31:27 +0600

    get to post

--- routes/api.php
+++ routes/api.php
@@ -8,10 +8,10 @@ use App\Http\Controllers\TestController;
 
 Route::middleware(ProtectedApp::class)->group(function () {
 
-    Route::post('/payment', [PaymentController::class, 'createCheckout']);
+//    Route::post('/payment', [PaymentController::class, 'createCheckout']);
 
 //    Route::post('/payment/callbackURL', [PaymentController::class, 'paymentSuccess'])->name('payment.success');
 
     Route::post('payment/verify', [PaymentController::class, 'verifyPayment'])->name('payment.verify');
 });
-Route::get('test/payment/verify',[TestController::class, 'testVerify']);
+//Route::get('test/payment/verify',[TestController::class, 'testVerify']);
